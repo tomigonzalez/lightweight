270f6cee973101ebe5cd8621ba6017cba092e0b9
tomi <tomimateo2008@hormail.com>
2026-07-29 00:51:53 -0300

    asd

--- app/(dashboard)/rutinas/nueva/form.tsx
+++ app/(dashboard)/rutinas/nueva/form.tsx
@@ -13,6 +13,8 @@ import {
 
 import { useRouter } from "next/navigation";
 import { guardarPlanSemanalAction } from "./actions";
+import { FaBed } from "react-icons/fa";
+import { GiWeightLiftingUp } from "react-icons/gi";
 
 interface ExerciseBase {
   id: string;
@@ -68,6 +70,7 @@ function crearEstadoInicial(rutinasActuales: any[]): PlanEstado {
 
   return estado;
 }
+
 export default function PlanSemanalForm({
   exercisesBase,
   rutinasActuales,
@@ -78,7 +81,6 @@ export default function PlanSemanalForm({
   const router = useRouter();
   const [loading, setLoading] = useState(false);
 
-  // los días inician como descanso para obligar a darles un nombre o activarlos
   const [plan, setPlan] = useState<PlanEstado>(() =>
     crearEstadoInicial(rutinasActuales),
   );
@@ -152,12 +154,12 @@ export default function PlanSemanalForm({
 
   return (
     <div className="max-w-4xl mx-auto space-y-10 pb-20">
-      <header className="flex flex-row md:flex-row justify-between items-start md:items-center gap-4">
+      <header className="flex flex-row items-center sticky top-0 z-50 border-b border-zinc-800/80 bg-zinc-950/60 py-4 backdrop-blur-md justify-between gap-4">
         <div className="flex flex-col">
           <h1 className="text-3xl font-black italic uppercase tracking-tighter">
             MI <span className="text-yellow-400">RUTINA SEMANAL</span>
           </h1>
-          <p className="text-zinc-500 text-[10px] font-bold uppercase tracking-widest mt-1">
+          <p className="text-zinc-500 hidden sm:block text-[10px] font-bold uppercase tracking-widest mt-1">
             Editá tu rutina semanal cuando lo necesites.
           </p>
         </div>
@@ -173,8 +175,8 @@ export default function PlanSemanalForm({
         </button>
       </header>
 
-      <div className="space-y-6 ">
-        <h3 className="text-xs font-black uppercase tracking-widest text-zinc-500 flex items-center gap-2 ">
+      <div className="space-y-6">
+        <h3 className="text-xs font-black uppercase tracking-widest text-zinc-500 flex items-center gap-2">
           <FiCalendar className="text-yellow-400" /> Distribución por días
         </h3>
 
@@ -185,49 +187,70 @@ export default function PlanSemanalForm({
             <div
               key={dia.id}
               className={`bg-zinc-900 border rounded-3xl overflow-hidden transition-all ${
-                diaEstado.isDescanso
-                  ? "border-zinc-900/50 opacity-60"
-                  : "border-zinc-800"
+                diaEstado.isDescanso ? "border-zinc-900/50" : "border-zinc-800"
               }`}
             >
-              <div className="p-5 flex flex-col md:flex-row justify-between items-start md:items-center gap-4 border-b border-zinc-800/50 bg-zinc-900/50">
+              <div className="p-5 flex flex-col justify-between items-start md:items-center gap-4 border-b border-zinc-800/50 bg-zinc-900/50">
                 <div className="flex items-center gap-4 flex-1 w-full">
-                  <span className="w-12 h-12 bg-black border border-zinc-800 rounded-xl flex items-center justify-center font-black italic text-yellow-400 text-sm">
-                    {dia.name.substring(0, 2).toUpperCase()}
-                  </span>
-                  <div className="flex-1">
-                    <h4 className="font-black italic uppercase text-lg">
-                      {dia.name}
-                    </h4>
-                    {!diaEstado.isDescanso ? (
-                      <input
-                        type="text"
-                        value={diaEstado.name}
-                        onChange={(e) =>
-                          handleNombreRutinaChange(dia.id, e.target.value)
-                        }
-                        placeholder="Ej: Empuje / Piernas"
-                        className="bg-transparent text-sm border-b border-zinc-800 focus:border-yellow-400 outline-none font-bold text-white w-full py-1 max-w-xs transition-colors"
-                      />
-                    ) : (
-                      <p className="text-zinc-600 text-[10px] font-bold uppercase tracking-tighter">
-                        Estado: Descanso Total
-                      </p>
-                    )}
-                  </div>
-                </div>
+                  <div
+                    className={`flex items-center gap-4 flex-1 transition-opacity duration-300 ${
+                      diaEstado.isDescanso
+                        ? "opacity-40 pointer-events-none"
+                        : "opacity-100"
+                    }`}
+                  >
+                    <span className="w-12 h-12 bg-black border border-zinc-800 rounded-xl flex items-center justify-center font-black italic text-yellow-400 text-sm">
+                      {dia.name.substring(0, 2).toUpperCase()}
+                    </span>
+                    <div className="w-full">
+                      <h4 className="font-black italic uppercase text-lg">
+                        {dia.name}
+                      </h4>
 
-                <div className="flex w-full md:w-auto gap-2 items-center">
+                      {!diaEstado.isDescanso ? (
+                        <input
+                          type="text"
+                          value={diaEstado.name}
+                          onChange={(e) =>
+                            handleNombreRutinaChange(dia.id, e.target.value)
+                          }
+                          placeholder="Ej: Empuje / Piernas"
+                          className="bg-transparent text-sm border-b border-zinc-800 focus:border-yellow-400 outline-none font-bold text-white w-full py-1 max-w-xs transition-colors"
+                        />
+                      ) : (
+                        <p className="text-zinc-500 text-[10px] font-bold uppercase tracking-tighter">
+                          Estado: Descanso Total
+                        </p>
+                      )}
+                    </div>
+                  </div>
+
                   <button
                     type="button"
                     onClick={() => toggleDescanso(dia.id)}
-                    className="flex-1 md:flex-none bg-zinc-800 hover:bg-zinc-700 text-white px-4 py-2 rounded-lg text-[10px] font-black uppercase tracking-widest transition-colors whitespace-nowrap"
+                    className="shrink-0 cursor-pointer bg-zinc-800 hover:bg-zinc-700 text-white px-4 py-2 rounded-lg transition-colors flex items-center justify-center gap-2 z-10"
                   >
-                    {diaEstado.isDescanso ? "Activar Día" : "Marcar Descanso"}
-                  </button>
+                    <span className="md:hidden">
+                      {diaEstado.isDescanso ? (
+                        <GiWeightLiftingUp className="text-lg text-green-400" />
+                      ) : (
+                        <FaBed className="text-lg" />
+                      )}
+                    </span>
 
-                  {!diaEstado.isDescanso && (
-                    <div className="relative bg-zinc-950 px-3 py-2 rounded-lg border border-zinc-800 flex items-center gap-2">
+                    <span className="hidden md:inline text-[10px] font-black uppercase tracking-widest whitespace-nowrap">
+                      {diaEstado.isDescanso ? (
+                        <p className="text-green-400">Activar Día </p>
+                      ) : (
+                        <p>Marcar Descanso</p>
+                      )}
+                    </span>
+                  </button>
+                </div>
+
+                {!diaEstado.isDescanso && (
+                  <div className="flex w-full flex-col gap-2 items-center">
+                    <div className="relative w-full bg-zinc-950 px-3 py-2 rounded-lg border border-zinc-800 flex items-center gap-2">
                       <FiPlus className="text-yellow-400" />
                       <select
                         onChange={(e) => {
@@ -235,7 +258,7 @@ export default function PlanSemanalForm({
                           e.target.value = "";
                         }}
                         defaultValue=""
-                        className="bg-transparent text-[10px] font-black uppercase tracking-widest text-zinc-400 outline-none cursor-pointer pr-2"
+                        className="w-full bg-transparent text-[10px] font-black uppercase tracking-widest text-zinc-400 outline-none cursor-pointer pr-2"
                       >
                         <option value="" disabled>
                           Agregar Ejercicio
@@ -251,21 +274,27 @@ export default function PlanSemanalForm({
                         ))}
                       </select>
                     </div>
-                  )}
-                </div>
+                  </div>
+                )}
               </div>
 
-              <div className="p-4 space-y-2 bg-black/20">
+              <div
+                className={`p-4 space-y-2 bg-black/20 transition-opacity duration-300 ${
+                  diaEstado.isDescanso
+                    ? "opacity-40 pointer-events-none"
+                    : "opacity-100"
+                }`}
+              >
                 {diaEstado.isDescanso ? (
-                  <div className="p-4 text-xs font-bold text-zinc-600 italic uppercase">
-                    🔋 Creciendo en el sillón. Hoy no se entrena.
+                  <div className="p-4 text-xs font-bold text-zinc-500 italic uppercase">
+                    🔋 Descanso. Hoy no se entrena.
                   </div>
                 ) : diaEstado.exercises.length === 0 ? (
                   <div className="flex items-center gap-4 p-4 bg-black/40 rounded-2xl border border-zinc-800/50">
                     <FiClock className="text-zinc-700" />
                     <span className="text-sm font-bold text-zinc-500 italic">
-                      No hay ejercicios agregados aún. Escribí un nombre arriba
-                      y agregá movimientos.
+                      No hay ejercicios agregados aún. Elegi y agrega
+                      movimientos.
                     </span>
                   </div>
                 ) : (
