4bc1b8d434cff9069a20202f579e6a7bd2f0b943
tomi <tomimateo2008@hormail.com>
2026-08-11 15:02:19 -0300

    loader UI

---
+++ app/(dashboard)/loading.tsx
@@ -0,0 +1,5 @@
+import GlobalLoading from "../components/Load/GlobalLoading";
+
+export default function Loading() {
+  return <GlobalLoading />;
+}
---
+++ app/(dashboard)/rutinas/loading.tsx
@@ -0,0 +1,5 @@
+import GlobalLoading from "@/app/components/Load/GlobalLoading";
+
+export default function Loading() {
+  return <GlobalLoading />;
+}
